204c50daffcc7de182d5d3943cb47d22783a7738
Kyle Zucker <zkyle12@gmail.com>
2017-03-20 20:32:17 -0600

    add runtime storage object into API helper

--- src/apiHelper.js
+++ src/apiHelper.js
@@ -1,13 +1,20 @@
 export default class APIHelper {
   constructor() {
     this.baseURL = 'https://swapi.co/api/'
+    this.store = {}
   }
 
   getAll(catagory, callback) {
+    if (this.store[catagory]) {
+      console.log('no need for API!')
+      return this.store[catagory]
+    }
+    console.log('had to hit API')
     //fetch(api/catagory)
     fetch(`${this.baseURL}${catagory}`)
       .then((response) => {
-        return response.json()
+        this.store[catagory] = response.json()
+        return this.store[catagory]
       })
       .then((result) => {
         callback(result)
